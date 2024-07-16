85455dfb826f80e56d0178d0262ae42700b366e6
ryeola <ryeolaisapain@gmail.com>
2024-07-15 19:02:16 -0500

    tiny fix

--- ui/rewrite.lua
+++ ui/rewrite.lua
@@ -29,7 +29,7 @@ local Library = {
 
     HudRegistry = {};
 
-    AccentColor = Redacted.Accent;
+    AccentColor = Color3.fromRGB(140, 130, 255)
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
