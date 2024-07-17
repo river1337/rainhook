fe35e1f3666940dbb719df7b08ad90ff6b95ad94
river <161112493+river1337@users.noreply.github.com>
2024-07-16 23:07:17 -0500

    Update Shared.lua

--- shared/Shared.lua
+++ shared/Shared.lua
@@ -1,26 +1,26 @@
 local Shared = {
-    _b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
-
-    encode = function(self, data)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    function encode(data)
         return ((data:gsub('.', function(x) 
             local r, b = '', x:byte()
             for i = 8, 1, -1 do r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and '1' or '0') end
-            return r
+            return r;
         end) .. '0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
             if (#x < 6) then return '' end
             local c = 0
             for i = 1, 6 do c = c + (x:sub(i, i) == '1' and 2 ^ (6 - i) or 0) end
-            return self._b:sub(c + 1, c + 1)
+            return b:sub(c + 1, c + 1)
         end) .. ({ '', '==', '=' })[#data % 3 + 1])
-    end,
-
-    decode = function(self, data)
-        data = string.gsub(data, '[^'..self._b..'=]', '')
+    end
+    
+    -- Base64 Decoding
+    function decode(data)
+        data = string.gsub(data, '[^'..b..'=]', '')
         return (data:gsub('.', function(x)
             if (x == '=') then return '' end
-            local r, f = '', (self._b:find(x) - 1)
+            local r, f = '', (b:find(x) - 1)
             for i = 6, 1, -1 do r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and '1' or '0') end
-            return r
+            return r;
         end):gsub('%d%d%d%d%d%d%d%d', function(x)
             local c = 0
             for i = 1, 8 do c = c + (x:sub(i, i) == '1' and 2 ^ (8 - i) or 0) end
