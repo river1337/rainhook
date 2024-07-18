411e81bcb208ddc5ed7ce68490ebb55770f862ac
ryeola <ryeolaisapain@gmail.com>
2024-07-17 23:31:58 -0500

    pp

--- ui/library.lua
+++ ui/library.lua
@@ -4620,11 +4620,9 @@ function library:CreateSettingsTab(menu)
     local mainSection = settingsTab:AddSection('Main', 1);
     local creditsSection = settingsTab:AddSection('Credits', 2);
     creditsSection:AddSeparator({text = 'Owners/Developers'});
-    creditsSection:AddText({text = "xz#1111"})
-    creditsSection:AddText({text = "goof#1000"})
+    creditsSection:AddText({text = "river (face.shxt)"})
     creditsSection:AddSeparator({text = 'Helpers'});
-    creditsSection:AddText({text = "encode#9999"})
-    creditsSection:AddText({text = "Vault#5434"})
+    creditsSection:AddText({text = "creida is god"})
 
 
     configSection:AddBox({text = 'Config Name', flag = 'configinput'})
